b455e5994cbee2dd487adabd17e4d8a22ff395f3
Aldrit <aldritkuleta2@gmail.com>
2025-01-22 08:06:40 +0100

    added delete and update crud

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,12 @@ const app = express();
 
 app.use(express.json()); //allow us to accept JSON data in the req.body
 
+/* 
+FONCTION SYNCRHONE
+Dans une fonction synchrone, le code est exécuté ligne par ligne, 
+dans l'ordre où il est écrit. Chaque ligne de code doit être exécutée avant de passer à la suivante.
+*/
+
 app.get("/api/products", async (req, res) =>{
     try {
         const products = await Product.find({});
@@ -43,10 +49,27 @@ app.delete("/api/products/:id", async (req, res) => {
         await Product.findByIdAndDelete(id);
         res.status(200).json({success:true, message:"Product deleted"});
     } catch (error) {
+        console.log("error in deleting product", error.message);
         res.status(404).json({success:false, message:"Product not found"});
     }
 });
 
+app.put("/api/products/:id", async (req, res)=>{
+    const {id} = req.params;
+
+    const product = req.body;
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({success:false, message:"Invalid product Id"});
+    }
+
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(id, product, {new:true});
+        res.status(200).json({success:true, data:updatedProduct});
+    } catch (error) {
+        res.status(500).json({success:false, message:"Server error"});
+    }
+})
 
 app.listen(5000, () => {
     connectDB();
